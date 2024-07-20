f5fa51042cec77f342559fb5463054780dcd9efb
BelloCM <kekanacm@gmail.com>
2024-07-20 22:41:47 +0200

    added js on select

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,47 @@
+function updateTime() {
+  let pacificElement = document.querySelector("#pacific");
+  if (pacificElement) {
+    let pacificDateElement = pacificElement.querySelector(".date");
+    let pacificTimeElement = pacificElement.querySelector(".time");
+    let pacificTime = moment().tz("Canada/pacific");
+
+    pacificDateElement.innerHTML = moment().format("dddd, Do , YYYY");
+    pacificTimeElement.innerHTML = pacificTime.format(
+      "H:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let johannesburgElement = document.querySelector("#johannesburg");
+  if (johannesburgElement) {
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+
+    johannesburgDateElement.innerHTML = moment().format("dddd, Do , YYYY");
+    johannesburgTimeElement.innerHTML = moment().format("H:mm:ss [<small>]A");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+};
+
+updateTime();
+setInterval(updateTime,1000);
 
 
-setInterval(function () {
-  let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
-
-  let pretoriaElement = document.querySelector("#pretoria");
-  let pretoriaDateElement = pretoriaElement.querySelector(".date");
-  let pretoriaTimeElement = pretoriaElement.querySelector(".time");
-
-  newYorkDateElement.innerHTML = moment().format("dddd, Do , YYYY");
-  newYorkTimeElement.innerHTML = newYorkTime.format(
-    "H:mm:ss [<small>]A[</small>]"
-  );
-
-  pretoriaDateElement.innerHTML = moment().format("dddd, Do , YYYY");
-  pretoriaTimeElement.innerHTML = moment().format("H:m:ss [<small>]A");
-});
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity,);
